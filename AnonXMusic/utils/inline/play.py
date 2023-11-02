5eb74b5a007f9e82e94f4f7bdafaf24b699da33d
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-11-03 00:08:06 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -33,32 +33,32 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "---started streaming---"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "---streaming now---"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "---Musiqo---"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "---via youtube---"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "---Join voice chat---"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "---Use /play to queue---"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "---Lag/ads free---"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "---Processing queue if any---"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "---Almost done---"
     else:
-        bar = "—————————◉"
+        bar = "---streaming next music on queue---"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="Reaume", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="Pause", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="Replay music", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="Skip music", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="End stream", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
@@ -74,11 +74,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="Resume", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="Pause", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="Replay music", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="Skip music", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="End stream", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -140,7 +140,7 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
         [
             InlineKeyboardButton(
-                text="◁",
+                text="Backward",
                 callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
@@ -148,7 +148,7 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
                 callback_data=f"forceclose {query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="▷",
+                text="Forward",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
         ],
