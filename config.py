a0ad2ce93bddf77f33435141df3dacc19ef5cef3
Lᴜᴄɪᴅᴏ™ <74089102+LucidoXD@users.noreply.github.com>
2023-11-03 08:42:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,10 +81,10 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://telegra.ph/file/a214852e89411aabddc00.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/a214852e89411aabddc00.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/a214852e89411aabddc00.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
